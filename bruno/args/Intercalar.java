c31a13386e3ad5817b8e1ee0be3f01cc688f0bcb
Bruno Raphael Perez <150837209+braphael98@users.noreply.github.com>
2024-02-07 00:03:35 +0000

    Intercalar

--- bruno/args/Intercalar.java
+++ bruno/args/Intercalar.java
@@ -1,7 +1,18 @@
-import java.util.Arrays;
 class Intercalar{
-    public static void main (String[]args){
-        System.out.println(Arrays.deepToString(args));
+    public static void main (String[] args){
+        int i = 0;
+        while (i < args.length){
+            if (i < args.length - 1){
+                System.out.print(args[i] + "-");
+                i++;
+            }
+            else {
+                System.out.print(args[i]);
+                i++;
+            }
+        }
+        System.out.println();
     }
+
 
 }
